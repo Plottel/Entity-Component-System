15fa4fc51df1c96814969abd92100735d0dfe9ba
Matt Noone <matt.noone95@gmail.com>
2016-10-11 21:40:11 +1100

    More documentation

--- src/Systems/ProjectileSystem.cs
+++ src/Systems/ProjectileSystem.cs
@@ -4,18 +4,40 @@ using SwinGameSDK;
 
 namespace MyGame
 {
+    /// <summary>
+    /// Represents the generic Projectile handler system. Checks if Projectiles have reached their
+    /// target and, if they have, removes them from the world. This System only processes generic
+    /// projectiles - all specific projectiles will be handled in Systems which inherit from this.
+    /// </summary>
     public class ProjectileSystem : System
     {
-        //This system only processes generic projectiles - all specific projectiles are excluded
-        public ProjectileSystem (World world) : base(new List<Type> {typeof(CProjectile), typeof(CPosition), typeof(CVelocity)}, new List<Type> {typeof(CFreezingBullet)}, world)
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:MyGame.ProjectileSystem"/> class.
+        /// </summary>
+        /// <param name="world">The World the System belongs to</param>
+        public ProjectileSystem (World world) : base(new List<Type> {typeof(CProjectile), typeof(CPosition), typeof(CVelocity)}, 
+                                                     new List<Type> {typeof(CFreezingBullet)}, 
+                                                     world)
         {
         }
 
-        protected bool ReachedTarget(CProjectile entProjectile, CPosition entPos)
+        /// <summary>
+        /// Specifies whether or not the projectile has reached its target.
+        /// </summary>
+        /// <returns>The target.</returns>
+        /// <param name="entTarget">Ent projectile.</param>
+        /// <param name="entPos">Ent position.</param>
+        protected bool ReachedTarget(CProjectile entTarget, CPosition entPos)
         {
-            return SwinGame.RectanglesIntersect(entProjectile.Target.Rect, entPos.Rect);
+            return SwinGame.RectanglesIntersect(entTarget.Target.Rect, entPos.Rect);
         }
 
+        /// <summary>
+        /// Removes all projectiles which have reached their target from the World.
+        /// This is done after the Process method in a separate list in order to prevent issues
+        /// that arise when modifying a list while looping through it.
+        /// </summary>
+        /// <param name="toRemove">The list of projectiles to remove.</param>
         protected void RemoveDeadProjectiles(List<int> toRemove)
         {
             foreach (int projectile in toRemove)
@@ -24,6 +46,10 @@ namespace MyGame
             }
         }
 
+        /// <summary>
+        /// Uses the projectile and position components of each Entity to determine whether 
+        /// or not the Entity has reached its target.
+        /// </summary>
         public override void Process()
         {
             List<int> deadProjectiles = new List<int>();
--- src/Systems/SpawningSystem.cs
+++ src/Systems/SpawningSystem.cs
@@ -4,27 +4,66 @@ using SwinGameSDK;
 
 namespace MyGame
 {
+    /// <summary>
+    /// Represents the System that spawns enemy units in the game. It spawns units at every interval,
+    /// determined by the World's Game Time.
+    /// </summary>
     public class SpawningSystem : System
     {
-        private const int _spawnAtX = 700;
+        /// <summary>
+        /// The x coordinate where units will be spawned.
+        /// </summary>
+        private const int _spawnAtX = 1200;
+
+        /// <summary>
+        /// The y coordinate where units will be spawned.
+        /// </summary>
         private int _spawnAtY;
-        private Random _random;
+
+        /// <summary>
+        /// Seed used to randomise y coordinate spawn locations.
+        /// </summary>
+        private Random _seed = new Random();
+
+        /// <summary>
+        /// How often the System will spawn units.
+        /// </summary>
         private uint _spawnInterval;
+
+        /// <summary>
+        /// When the System last spawned. Used to determine when the next spawn will occur.
+        /// </summary>
         private uint _lastSpawn;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:MyGame.SpawningSystem"/> class.
+        /// This System does not contain any Entities, it simply spawns units.
+        /// </summary>
+        /// <param name="world">The World the System belongs to.</param>
         public SpawningSystem (World world) : base(new List<Type> {}, new List<Type> {}, world)
         {
-            _random = new Random();
             _spawnInterval = 500;
         }
 
+        /// <summary>
+        /// Specifies whether or not the System is ready to spawn units.
+        /// This uses the Game Time to see if enough time has passed since the last spawn.
+        /// </summary>
+        private bool ReadyToSpawn()
+        {
+            return World.GameTime - _lastSpawn >= _spawnInterval;
+        }
+
+        /// <summary>
+        /// Spawns one of each unit type at random Y locations if the System is Ready To Spawn
+        /// </summary>
         public override void Process()
         {
-            if (World.GameTime - _lastSpawn >= _spawnInterval)
+            if (ReadyToSpawn())
             {
                 _lastSpawn = World.GameTime;
 
-                _spawnAtY = _random.Next(50, 550);
+                _spawnAtY = _seed.Next(50, 550);
                 EntityFactory.CreateSwordMan(_spawnAtX, _spawnAtY);
                 EntityFactory.CreateBatteringRam(_spawnAtX, _spawnAtY + 50);
                 EntityFactory.CreateEnemyArcher(_spawnAtX, _spawnAtY - 50);
